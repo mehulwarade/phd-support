2c98a992bb6b45234d14a6c7c3c5be56be4d33ad
mehulwarade <mehulwarade@gmail.com>
2022-02-23 22:04:22 +1100

    folders for other genome created

---
+++ genome_data/40k/40k_mysql_test.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+for workflow_size in 50
+do
+	mkdir output
+	mkdir output/$workflow_size
+	cd $workflow_size
+
+	for index_node in $(seq 1 12); 
+	do
+
+		jobstate_file=$index_node'n_i'$workflow_size'_40k-jobstate.log'
+		mysql_table=$index_node'n_i'$workflow_size'_40k'
+
+		echo 'Processing: ' $jobstate_file ' using table: ' $mysql_table
+
+		# echo 'size: ' $workflow_size ' nodes: ' $index_node
+		start=$(awk '{print $1}' $index_node'n_i'$workflow_size'_40k-jobstate.log' | head -n 1)
+    	end=$(awk '{print $1}' $index_node'n_i'$workflow_size'_40k-jobstate.log' | tail -n 1)
+
+		for time in $(seq $start $end); 
+		do 
+			echo "File: " $jobstate_file " Time executed: " $time
+			#mysql -u root -pz 20k_genome -ss -e "select * from $mysql_table where timestamp = $time" >> ../output/$workflow_size/"$mysql_table"".csv"
+		done
+		
+		echo 'End Processing: ' $index_node'n_i'$workflow_size'_40k-jobstate.log'
+	done
+done
