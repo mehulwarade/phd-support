dab69fd563a3a8d1dc107884ff1d166c48ebc308
mehulwarade <mehulwarade@gmail.com>
2022-02-24 12:18:04 +1100

    genome changed from 20-30-40 to 10-20-30

--- genome_data/10k/10k_mysql_test.sh
+++ genome_data/10k/10k_mysql_test.sh
@@ -9,19 +9,19 @@ do
 	for index_node in $(seq 1 12); 
 	do
 
-		jobstate_file=$index_node'n_i'$workflow_size'_40k-jobstate.log'
-		mysql_table=$index_node'n_i'$workflow_size'_40k'
+		jobstate_file=$index_node'n_i'$workflow_size'_10k-jobstate.log'
+		mysql_table=$index_node'n_i'$workflow_size'_10k'
 
 		echo 'Processing: ' $jobstate_file ' using table: ' $mysql_table
 
 		# echo 'size: ' $workflow_size ' nodes: ' $index_node
-		start=$(awk '{print $1}' $index_node'n_i'$workflow_size'_40k-jobstate.log' | head -n 1)
-    	end=$(awk '{print $1}' $index_node'n_i'$workflow_size'_40k-jobstate.log' | tail -n 1)
+		start=$(awk '{print $1}' $jobstate_file | head -n 1)
+    	end=$(awk '{print $1}' $jobstate_file | tail -n 1)
 
 		for time in $(seq $start $end); 
 		do 
 			echo "File: " $jobstate_file " Time executed: " $time
-			#mysql -u root -pz 20k_genome -ss -e "select * from $mysql_table where timestamp = $time" >> ../output/$workflow_size/"$mysql_table"".csv"
+			mysql -u root -pz phd -ss -e "select * from $mysql_table where timestamp = $time" >> output/$workflow_size/"$mysql_table"".csv"
 		done
 		
 		echo 'End Processing: ' $index_node'n_i'$workflow_size'_40k-jobstate.log'
