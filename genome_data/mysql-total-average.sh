df6c7cd533be56b15a747bbefba64c7b8b6a1b80
mehulwarade <mehulwarade@gmail.com>
2022-03-01 07:53:38 +1100

    i50-20k data done and little bit others done

---
+++ genome_data/mysql-total-average.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+select 
+
+mysql -u root -pz phd -ss -e "select timestamp,description,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21,p22,p23,p24,(p1+p2+p3+p4+p5+p6+p8+p9+p10+p11+p12+p13+p14) AS TOTAL FROM teest";
+
+
+# Get total of every row inot a column
+# Get the average of the column
+
+
+select sum(p1+p2+p3+p4+p5+p6+p8+p9+p10+p11+p12+p13+p14) AS TOTAL FROM teest;
