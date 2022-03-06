ce98aa7571ecd86b4fce5d99b86202ef6c77c3da
mehulwarade <mehulwarade@gmail.com>
2022-03-06 11:53:33 +1100

    10k data extracted and all data done

--- genome_data/10k/10k_mysql_test.sh
+++ genome_data/10k/10k_mysql_test.sh
@@ -1,32 +1,66 @@
 #!/bin/sh
 
-for workflow_size in 50
+total_columns=14
+nodes=12
+individual=50
+
+j='i50'
+
+echo ====================================== >> runtime.log
+
+for i in $(seq 1 $nodes)
 do
-	mkdir output
-	mkdir output/$workflow_size
-	cd $workflow_size
-	echo ====================================== >> runtime.log
-	for index_node in $(seq 1 12); 
-	do
+	echo $i
 
-		jobstate_file=$index_node'n_i'$workflow_size'_10k-jobstate.log'
-		mysql_table=$index_node'n_i'$workflow_size'_10k'
+	jobstate_file=$i'n_i'$individual'_10k-jobstate.log'
+	mysql_table=$i'n_i'$individual'_10k'
 
-		echo 'Processing: ' $jobstate_file ' using table: ' $mysql_table
+	start=$(awk '{print $1}' $j/$i/$jobstate_file | head -n 1)
+	end=$(awk '{print $1}'  $j/$i/$jobstate_file | tail -n 1)
 
-		# echo 'size: ' $workflow_size ' nodes: ' $index_node
-		start=$(awk '{print $1}' $jobstate_file | head -n 1)
-    	end=$(awk '{print $1}' $jobstate_file | tail -n 1)
+	echo 'Processing: ' $jobstate_file ' using table: ' $mysql_table
+	
+	echo $mysql_table: Runtime: $( expr $end - $start )  >> runtime.log
 
-		
-		echo $jobstate_file $( expr $end - $start )  >> runtime.log
+	if [ $i -eq 1 ]
+	then
+	mysql -u root -pz phd -ss -e "select avg(p1) AS TOTAL FROM $mysql_table where timestamp between $start and $end" >> runtime.log
+	# echo $mysql_table: Average: $avg >> runtime.log
+	elif [ $i -eq 2 ]
+	then
+	mysql -u root -pz phd -ss -e "select avg(p1+p2) AS TOTAL FROM $mysql_table where timestamp between $start and $end" >> runtime.log
+	elif [ $i -eq 3 ]
+	then
+	mysql -u root -pz phd -ss -e "select avg(p1+p2+p3) AS TOTAL FROM $mysql_table where timestamp between $start and $end" >> runtime.log
+	elif [ $i -eq 4 ]
+	then
+	mysql -u root -pz phd -ss -e "select avg(p1+p2+p3+p4) AS TOTAL FROM $mysql_table where timestamp between $start and $end" >> runtime.log
+	elif [ $i -eq 5 ]
+	then
+	mysql -u root -pz phd -ss -e "select avg(p1+p2+p3+p4+p5) AS TOTAL FROM $mysql_table where timestamp between $start and $end" >> runtime.log
+	elif [ $i -eq 6 ]
+	then
+	mysql -u root -pz phd -ss -e "select avg(p1+p2+p3+p4+p5+p6) AS TOTAL FROM $mysql_table where timestamp between $start and $end" >> runtime.log
+	elif [ $i -eq 7 ]
+	then
+	mysql -u root -pz phd -ss -e "select avg(p1+p2+p3+p4+p5+p6+p9) AS TOTAL FROM $mysql_table where timestamp between $start and $end" >> runtime.log
+	elif [ $i -eq 8 ]
+	then
+	mysql -u root -pz phd -ss -e "select avg(p1+p2+p3+p4+p5+p6+p9+p10) AS TOTAL FROM $mysql_table where timestamp between $start and $end" >> runtime.log
+	elif [ $i -eq 9 ]
+	then
+	mysql -u root -pz phd -ss -e "select avg(p1+p2+p3+p4+p5+p6+p9+p10+p11) AS TOTAL FROM $mysql_table where timestamp between $start and $end" >> runtime.log
+	elif [ $i -eq 10 ]
+	then
+	mysql -u root -pz phd -ss -e "select avg(p1+p2+p3+p4+p5+p6+p9+p10+p11+p12) AS TOTAL FROM $mysql_table where timestamp between $start and $end" >> runtime.log
+	elif [ $i -eq 11 ]
+	then
+	mysql -u root -pz phd -ss -e "select avg(p1+p2+p3+p4+p5+p6+p9+p10+p11+p12+p13) AS TOTAL FROM $mysql_table where timestamp between $start and $end" >> runtime.log
+	elif [ $i -eq 12 ]
+	then
+	mysql -u root -pz phd -ss -e "select avg(p1+p2+p3+p4+p5+p6+p9+p10+p11+p12+p13+p14) AS TOTAL FROM $mysql_table where timestamp between $start and $end" >> runtime.log
+	else
+	echo haha
+	fi
 
-		for time in $(seq $start $end); 
-		do 
-			echo "File: " $jobstate_file " Time executed: " $time
-			mysql -u root -pz phd -ss -e "select * from $mysql_table where timestamp = $time" >> output/$workflow_size/"$mysql_table"".csv"
-		done
-		
-		echo 'End Processing: ' $index_node'n_i'$workflow_size'_40k-jobstate.log'
-	done
 done
